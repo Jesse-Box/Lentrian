7aa60c75a605b1d7c1b411ff33ef19dcc74fa937
Jesse Box <me@jessebox.net>
2014-03-18 22:00:08 +0100

    MAX IMAGE 2 Plugin
    Added the JQUERY plugin called MAX IMAGE into the working file but have
    yet to use it. Also started the nave.

--- index.php
+++ index.php
@@ -3,28 +3,42 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>LENTRIAN</title>
+    <meta name="description" content="">
+    <!--<meta name="viewport" content="width=device-width">-->
+    <link rel="stylesheet" href="assests/css/reset.css" type="text/css">
+    <link rel="stylesheet" href="assests/css/main.css" type="text/css">
+    <link rel="stylesheet" href="assests/css/print.css" type="text/css" media="print">
+    <link rel="stylesheet" href="styles/jquery.maximage.css" type="text/css" media="screen" title="CSS" charset="utf-8">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
+    <script src="assests/js/vendor/modernizr-2.6.2.min.js"></script>
+    <script src="assests/js/jquery.cycle.all.js" type="text/javascript"></script>
+    <script src="assests/js/jquery.maximage.min.js" type="text/javascript"></script>
+  </head>
+  <body>
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+    <section id="container">
+      <nav class="top-nav">
+        <ul>
+          <li><a href="#">collections</a></li>
+          <li><a href="#">acquire</a></li>
+        </ul>
+      </nav>
+      <p>Hello world! This is HTML5 Boilerplate.</p>
+      <nav class="bottom-nav">
+        <ul>
+          <li><a href="#">various</a></li>
+          <li><a href="#">contact</a></li>
+        </ul>
+      </nav>      
+    </section>
 
-        <link rel="stylesheet" href="css/normalize.min.css">
-        <link rel="stylesheet" href="css/main.css">
-
-        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
-    </head>
-    <body>
-        <!--[if lt IE 7]>
-            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-        <![endif]-->
-
-        <p>Hello world! This is HTML5 Boilerplate.</p>
-
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
-
-        <script src="js/main.js"></script>
-    </body>
+    
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+  </body>
 </html>
