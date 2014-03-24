5424c3f87bf8355c369978f1a5f3844862a28500
Jesse Box <me@jessebox.net>
2014-03-24 14:47:43 +0100

    Implementation of Fresco Lightbox
    Got the correct Behavioural effect I need for this project.

--- assests/inc/footer.php
+++ assests/inc/footer.php
@@ -1,36 +1,56 @@
-  </section>
-  <nav class="bottom-nav">
-    <ul>
-      <li><a href="#">various</a></li>
-      <li><a href="#">contact</a></li>
-    </ul>
-  </nav>
-  <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
-  <script type="text/javascript" charset="utf-8"> //Max Image Trigger
-    $(function() { 
-      var imgs = $('#maximage img');
-      imgs.sort(function() { return 0.5 - Math.random() });
-      $('#maximage').html( imgs );
-      
-      $('#maximage').maximage({
-          cycleOptions: {
-              speed: 800
-          }
-      });
-    });
-  </script>
-  <script type="text/javascript"> //Grid-a-licious Trigger
-      $(document).ready(function () {
-        $("#collections").gridalicious({
-            gutter: 20,
-            width: 400,
-            animate: true,
-            animationOptions: {
-                    speed: 200,
-                    duration: 300
-            },
+    </section>
+    <nav class="bottom-nav">
+      <ul>
+        <li><a href="#">various</a></li>
+        <li><a href="#">contact</a></li>
+      </ul>
+    </nav>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+    <script type="text/javascript" charset="utf-8"> //Max Image Trigger
+      $(function() { 
+        var imgs = $('#maximage img');
+        imgs.sort(function() { return 0.5 - Math.random() });
+        $('#maximage').html( imgs );
+        
+        $('#maximage').maximage({
+            cycleOptions: {
+                speed: 800
+            }
         });
       });
-  </script>
+    </script>
+    <script type="text/javascript"> //Grid-a-licious Trigger
+        $(document).ready(function () {
+          $("#collections").gridalicious({
+              gutter: 20,
+              width: 400,
+              animate: true,
+              animationOptions: {
+                      speed: 200,
+                      duration: 300
+              },
+          });
+        });
+    </script>
+    <script type="text/javascript"> //Fresco Trigger
+      jQuery(document).ready(function($) {
+
+        $('#collection-1').bind('click', function(event) {
+          // stop default click behavior
+          event.preventDefault();
+
+          Fresco.show([
+            {
+            url: '../assests/img/Landscape-1-thumb.jpg',
+            caption: "Landscape-1-thumb" }, 
+
+            {
+            url: '../assests/img/5-thumb.jpg',
+            caption: "5-thumb.jpg" }
+            ]);
+        });
+        
+      });
+    </script>
   </body>
 </html>
--- pages/collections.php
+++ pages/collections.php
@@ -4,10 +4,9 @@ $Container = "collections";
 include('../assests/inc/header.php'); ?>
 
 <a 
-	href="../assests/img/Landscape-1-thumb.jpg" 
-	class="item fresco"
-	data-fresco-group="Collection-1"
-	data-fresco-group-options="thumbnails: false">
+	href="#" 
+	class="item"
+	id="collection-1">
   <img src="../assests/img/Landscape-1-thumb.jpg" "alt=collection I">
 </a>
 <a 
