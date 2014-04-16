c596c9018128e944d90ac2eb48d43b5653a4d53b
Jesse Box <me@jessebox.net>
2014-04-16 14:18:37 +0200

    contact.php
    seems like my last commit didn’t include the latest changes on my
    contact page

---
+++ contact.php
@@ -0,0 +1,10 @@
+<?php 
+$PageTitle = "LENTRIAN - CONTACT";
+$Container = "contact";
+include('inc/header.php'); ?>
+<h6>
+	<a href="mailto:sales@lentrian.com">Email</a> + 
+	<a href="https://www.facebook.com/Lentrian">Facebook</a>
+</h6>
+<?php 
+include('inc/footer.php'); ?>
