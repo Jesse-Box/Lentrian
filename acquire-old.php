0d65f3d0549c937520872fe7fe4e828873aaf2dd
Jesse Box <me@jessebox.net>
2014-11-02 21:53:43 +0100

    Content update
    Updating pictures and links to new garments

---
+++ acquire-old.php
@@ -0,0 +1,171 @@
+<section id="col-wrapper">
+	<section class="flex-col" id="col-1">
+		<a href="http://store.lentrian.com/products/lss14-pnt2">
+	  	<img src="img/acquire/Lentrian-web-pictures-4.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-vst2-black">
+	  	<img src="img/acquire/Lentrian-web-pictures-51.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-dnm2">
+	  	<img src="img/acquire/Lentrian-web-pictures-94.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-lea6">
+	  	<img src="img/acquire/Lentrian-web-pictures-46.jpg">
+		</a>
+	</section>
+	<section class="flex-col" id="col-2">
+		<a href="http://store.lentrian.com/products/lss14-pnt1">
+	  	<img src="img/acquire/Lentrian-web-pictures-8.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-vst1">
+	  	<img src="img/acquire/Lentrian-web-pictures-55.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-dnm2">
+	  	<img src="img/acquire/Lentrian-web-pictures-2.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-sht2">
+	  	<img src="img/acquire/Lentrian-web-pictures-50.jpg">
+		</a>
+	</a>
+	</section>
+	<section class="flex-col" id="col-3">
+		<a href="http://store.lentrian.com/products/lss14-dnm2-grey">
+	  	<img src="img/acquire/Lentrian-web-pictures-12.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-cdgn">
+	  	<img src="img/acquire/Lentrian-web-pictures-59-edit.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-pnt2">
+	  	<img src="img/acquire/Lentrian-web-pictures-3.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-vst2-black">
+	  	<img src="img/acquire/Lentrian-web-pictures-54.jpg">
+		</a>
+	</section>
+	<section class="flex-col" id="col-4">
+		<a href="http://store.lentrian.com/products/lss14-tnk2">
+	  	<img src="img/acquire/Lentrian-web-pictures-15.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-sht1">
+	  	<img src="img/acquire/Lentrian-web-pictures-65.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-pnt1">
+	  	<img src="img/acquire/Lentrian-web-pictures-7.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-vst1">
+	  	<img src="img/acquire/Lentrian-web-pictures-58.jpg">
+		</a>
+	</section>
+	<section class="flex-col" id="col-5">
+		<a href="http://store.lentrian.com/products/lss14-tee1">
+	  	<img src="img/acquire/Lentrian-web-pictures-18.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-tee2">
+	  	<img src="img/acquire/Lentrian-web-pictures-66.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-dnm2-grey">
+	  	<img src="img/acquire/Lentrian-web-pictures-11.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-cdgn">
+	  	<img src="img/acquire/Lentrian-web-pictures-62.jpg">
+		</a>
+	</section>
+	<section class="flex-col" id="col-6">
+		<a href="http://store.lentrian.com/products/lss14-cot1">
+	  	<img src="img/acquire/Lentrian-web-pictures-21.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-sht1-grey">
+	  	<img src="img/acquire/Lentrian-web-pictures-69-edit.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-tnk2">
+	  	<img src="img/acquire/Lentrian-web-pictures-16.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-sht1">
+	  	<img src="img/acquire/Lentrian-web-pictures-64.jpg">
+		</a>
+	</section>
+	<section class="flex-col" id="col-7">
+		<a href="http://store.lentrian.com/products/lss14-cot2">
+	  	<img src="img/acquire/Lentrian-web-pictures-26.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-jrs1-grey">
+	  	<img src="img/acquire/Lentrian-web-pictures-73.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-tee1">
+	  	<img src="img/acquire/Lentrian-web-pictures-19.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-tee2">
+	  	<img src="img/acquire/Lentrian-web-pictures-68.jpg">
+		</a>
+	</section>
+	<section class="flex-col" id="col-8">
+		<a href="http://store.lentrian.com/products/lss14-vst1-black">
+	  	<img src="img/acquire/Lentrian-web-pictures-33.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-lea2">
+	  	<img src="img/acquire/Lentrian-web-pictures-76.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-cot1">
+	  	<img src="img/acquire/Lentrian-web-pictures-24.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-sht1-grey">
+	  	<img src="img/acquire/Lentrian-web-pictures-72.jpg">
+		</a>
+	</section>
+	<section class="flex-col" id="col-9">
+		<a href="http://store.lentrian.com/products/lss14-jkt4">
+	  	<img src="img/acquire/Lentrian-web-pictures-37.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-vst2">
+	  	<img src="img/acquire/Lentrian-web-pictures-81.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-cot2">
+	  	<img src="img/acquire/Lentrian-web-pictures-30.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-jrs1-grey">
+	  	<img src="img/acquire/Lentrian-web-pictures-74.jpg">
+		</a>
+	</section>
+	<section class="flex-col" id="col-10">
+		<a href="http://store.lentrian.com/products/lss14-sht2-white">
+	  	<img src="img/acquire/Lentrian-web-pictures-41.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-jrs1">
+	  	<img src="img/acquire/Lentrian-web-pictures-84.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-vst1-black">
+	  	<img src="img/acquire/Lentrian-web-pictures-31.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-lea2">
+	  	<img src="img/acquire/Lentrian-web-pictures-79.jpg">
+		</a>
+	</section>
+	<section class="flex-col" id="col-11">
+		<a href="http://store.lentrian.com/products/lss14-lea6">
+	  	<img src="img/acquire/Lentrian-web-pictures-44.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-swt-grey">
+	  	<img src="img/acquire/Lentrian-web-pictures-87.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-jkt4">
+	  	<img src="img/acquire/Lentrian-web-pictures-40.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-vst2">
+	  	<img src="img/acquire/Lentrian-web-pictures-82.jpg">
+		</a>
+	</section>
+	<section class="flex-col" id="col-12">
+		<a href="http://store.lentrian.com/products/lss14-sht2">
+	  	<img src="img/acquire/Lentrian-web-pictures-47-edit.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-swt">
+	  	<img src="img/acquire/Lentrian-web-pictures-90.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-sht2-white">
+	  	<img src="img/acquire/Lentrian-web-pictures-43.jpg">
+		</a>
+		<a href="http://store.lentrian.com/products/lss14-jrs1">
+	  	<img src="img/acquire/Lentrian-web-pictures-86.jpg">
+		</a>
+	</section>
+</section>
