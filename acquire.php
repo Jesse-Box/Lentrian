dc85692ce50f71225cc0d557cc225172f150bdd5
Jesse Box <me@jessebox.net>
2014-11-09 22:59:02 +0100

    Neat & Bitters Experimentation
    Still need restructure my new bitters files into a base folder so it
    replaces my old typography stylings.

--- acquire.php
+++ acquire.php
@@ -3,209 +3,151 @@ $PageTitle = "LENTRIAN - ACQUIRE";
 $Container = "acquire";
 include('inc/header.php'); ?>
 
-<section id="col-wrapper">
-	<section class="flex-col" id="col-1">
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/asymmetrical-lamb-leather-jacket">
-	  	<img src="img/acquire/2-11-14/lentrian-web_19-2.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/loose-crotch-jersey-pants">
-	  	<img src="img/acquire/2-11-14/lentrian-web_59.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/heavy-grey-slim-leather-jacket">
-	  	<img src="img/acquire/2-11-14/lentrian-web_33.jpg">
-		</a>
-		<a href="http://store.lentrian.com/products/white-slim-shirt-with-thumb-holes">
-	  	<img src="img/acquire/11-04-14/Lentrian-web-pictures-41.jpg">
-		</a>
-	</section>
-	<section class="flex-col" id="col-2">
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/black-long-sleeve-turtle-neck-shirt">
-	  	<img src="img/acquire/2-11-14/lentrian-web_467.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/black-washed-leather-jacket">
-	  	<img src="img/acquire/2-11-14/lentrian-web_28-2.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/lss14-tnk2">
-	  	<img src="img/acquire/2-11-14/lentrian-web_122.jpg">
-		</a>
-		<a href="http://store.lentrian.com/products/lss14-vst2-black">
-	  	<img src="img/acquire/11-04-14/Lentrian-web-pictures-51.jpg">
-		</a>
+<section class="container">
+	<a class="shop-item" href="http://store.lentrian.com/products/asymmetrical-lamb-leather-jacket">
+  		<img src="img/acquire/2-11-14/lentrian-web_19-2.jpg">
 	</a>
-	</section>
-	<section class="flex-col" id="col-3">
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/grey-rough-texture-wool-pant">
-	  	<img src="img/acquire/2-11-14/lentrian-web_55.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/black-button-down-jersey-cardigan">
-	  	<img src="img/acquire/2-11-14/lentrian-web_70-2.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/object-dyed-linnen-overcoat">
-	  	<img src="img/acquire/2-11-14/lentrian-web_102.jpg">
-		</a>
-		<a href="http://store.lentrian.com/products/lss14-swt">
-	  	<img src="img/acquire/11-04-14/Lentrian-web-pictures-90.jpg">
-		</a>
-	</section>
-	<section class="flex-col" id="col-4">
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/black-leather-wool-coat">
-	  	<img src="img/acquire/2-11-14/lentrian-web_36.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/object-dyed-slim-shirt">
-	  	<img src="img/acquire/2-11-14/lentrian-web_74.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/lss14-swt-grey">
-	  	<img src="img/acquire/2-11-14/lentrian-web_22.jpg">
-		</a>
-		<a href="http://store.lentrian.com/products/double-buttoned-linen-jacket">
-	  	<img src="img/acquire/11-04-14/Lentrian-web-pictures-37.jpg">
-		</a>
-	</section>
-	<section class="flex-col" id="col-5">
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/bended-double-layered-t-shirt">
-	  	<img src="img/acquire/2-11-14/lentrian-web_479.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/object-dyed-heavy-cotton-pants">
-	  	<img src="img/acquire/2-11-14/lentrian-web_62.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/washed-leather-blazer-with-zippers">
-	  	<img src="img/acquire/2-11-14/lentrian-web_85.jpg">
-		</a>
-		<a href="http://store.lentrian.com/products/lss14-sht2">
-	  	<img src="img/acquire/11-04-14/Lentrian-web-pictures-47-edit.jpg">
-		</a>
-	</section>
-	<section class="flex-col" id="col-6">
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/high-neck-washed-leather-jacket">
-	  	<img src="img/acquire/2-11-14/lentrian-web_24-2.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/lss14-tee1">
-	  	<img src="img/acquire/2-11-14/lentrian-web_112.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/long-sleeve-top">
-	  	<img src="img/acquire/2-11-14/lentrian-web_483.jpg">
-		</a>
-		<a href="http://store.lentrian.com/products/grey-wrap-vest">
-	  	<img src="img/acquire/11-04-14/Lentrian-web-pictures-81.jpg">
-		</a>
-	</section>
-	<section class="flex-col" id="col-7">
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/black-twisted-denim">
-	  	<img src="img/acquire/2-11-14/lentrian-web_37.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/lss14-vst1">
-	  	<img src="img/acquire/2-11-14/lentrian-web_18.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/long-cardigan-with-leather-details">
-	  	<img src="img/acquire/2-11-14/lentrian-web_13-2.jpg">
-		</a>
-		<a href="http://store.lentrian.com/products/black-rough-linen-shirt-with-hidden-metal-zipper">
-	  	<img src="img/acquire/11-04-14/Lentrian-web-pictures-65.jpg">
-		</a>
-	</section>
-	<section class="flex-col" id="col-8">
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/double-layered-gatefold-cardigan">
-	  	<img src="img/acquire/2-11-14/lentrian-web_475.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/grey-heavy-wool-coat-with-rough-texture">
-	  	<img src="img/acquire/2-11-14/lentrian-web_55-2.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/rough-linen-shirt-with-hidden-metal-zipper-1">
-	  	<img src="img/acquire/2-11-14/lentrian-web_111-2.jpg">
-		</a>
-		<a href="http://store.lentrian.com/products/grey-object-dyed-linen-pants">
-	  	<img src="img/acquire/2-11-14/lentrian-web_49.jpg">
-		</a>
-	</section>
-	<section class="flex-col" id="col-9">
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/heavy-wool-coat-with-rough-texture">
-	  	<img src="img/acquire/2-11-14/lentrian-web_60-2.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/dyed-drop-crotch-linen-pants">
-	  	<img src="img/acquire/2-11-14/lentrian-web_29.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/double-layered-jersey">
-	  	<img src="img/acquire/2-11-14/lentrian-web_472.jpg">
-		</a>
-		<a href="http://store.lentrian.com/products/black-washed-leather-jacket">
-	  	<img src="img/acquire/2-11-14/lentrian-web_30-2.jpg">
-		</a>
-	</section>
-	<section class="flex-col" id="col-10">
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/grey-object-dyed-linen-pants">
-	  	<img src="img/acquire/2-11-14/lentrian-web_44.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/heavy-wool-jacket-with-leather-center-front">
-	  	<img src="img/acquire/2-11-14/lentrian-web_47-2.jpg">
-		</a>
-		<a href="http://store.lentrian.com/products/washed-black-leather-jacket">
-	  	<img src="img/acquire/11-04-14/Lentrian-web-pictures-76.jpg">
-		</a>
-		<a href="http://store.lentrian.com/products/black-leather-wool-coat">
-	  	<img src="img/acquire/2-11-14/lentrian-web_37-2.jpg">
-		</a>
-	</section>
-	<section class="flex-col" id="col-11">
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/long-black-single-button-overcoat">
-	  	<img src="img/acquire/2-11-14/lentrian-web_41-2.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/white-formal-shirt">
-	  	<img src="img/acquire/2-11-14/lentrian-web_79.jpg">
-		</a>
-		<a href="http://store.lentrian.com/products/grey-overdyed-twisted-denim">
-	  	<img src="img/acquire/11-04-14/Lentrian-web-pictures-12.jpg">
-		</a>
-		<a href="http://store.lentrian.com/products/hooded-double-layered-jersey">
-	  	<img src="img/acquire/2-11-14/lentrian-web_466.jpg">
-		</a>
-	</section>
-	<section class="flex-col" id="col-12">
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/double-layered-linen-coat">
-	  	<img src="img/acquire/2-11-14/lentrian-web_13.jpg">
-		</a>
-		<!--NEW 2.11.14-->
-		<a href="http://store.lentrian.com/products/hooded-double-layered-jersey">
-	  	<img src="img/acquire/2-11-14/lentrian-web_460.jpg">
-		</a>
-		<a href="http://store.lentrian.com/products/black-long-linen-vest-with-high-collar">
-	  	<img src="img/acquire/11-04-14/Lentrian-web-pictures-33.jpg">
-		</a>
-		<a href="http://store.lentrian.com/products/asymmetrical-lamb-leather-jacket">
-	  	<img src="img/acquire/2-11-14/lentrian-web_23-2.jpg">
-		</a>
-	</section>
+	<a class="shop-item" href="http://store.lentrian.com/products/loose-crotch-jersey-pants">
+  		<img src="img/acquire/2-11-14/lentrian-web_59.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/heavy-grey-slim-leather-jacket">
+  		<img src="img/acquire/2-11-14/lentrian-web_33.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/white-slim-shirt-with-thumb-holes">
+  		<img src="img/acquire/11-04-14/Lentrian-web-pictures-41.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/black-long-sleeve-turtle-neck-shirt">
+  		<img src="img/acquire/2-11-14/lentrian-web_467.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/black-washed-leather-jacket">
+  		<img src="img/acquire/2-11-14/lentrian-web_28-2.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/lss14-tnk2">
+  		<img src="img/acquire/2-11-14/lentrian-web_122.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/lss14-vst2-black">
+  		<img src="img/acquire/11-04-14/Lentrian-web-pictures-51.jpg">
+	</a>		
+	<a class="shop-item" href="http://store.lentrian.com/products/grey-rough-texture-wool-pant">
+  		<img src="img/acquire/2-11-14/lentrian-web_55.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/black-button-down-jersey-cardigan">
+  		<img src="img/acquire/2-11-14/lentrian-web_70-2.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/object-dyed-linnen-overcoat">
+  		<img src="img/acquire/2-11-14/lentrian-web_102.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/lss14-swt">
+  		<img src="img/acquire/11-04-14/Lentrian-web-pictures-90.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/black-leather-wool-coat">
+  		<img src="img/acquire/2-11-14/lentrian-web_36.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/object-dyed-slim-shirt">
+  		<img src="img/acquire/2-11-14/lentrian-web_74.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/lss14-swt-grey">
+  		<img src="img/acquire/2-11-14/lentrian-web_22.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/double-buttoned-linen-jacket">
+  		<img src="img/acquire/11-04-14/Lentrian-web-pictures-37.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/bended-double-layered-t-shirt">
+  		<img src="img/acquire/2-11-14/lentrian-web_479.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/object-dyed-heavy-cotton-pants">
+  		<img src="img/acquire/2-11-14/lentrian-web_62.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/washed-leather-blazer-with-zippers">
+  		<img src="img/acquire/2-11-14/lentrian-web_85.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/lss14-sht2">
+  		<img src="img/acquire/11-04-14/Lentrian-web-pictures-47-edit.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/high-neck-washed-leather-jacket">
+  		<img src="img/acquire/2-11-14/lentrian-web_24-2.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/lss14-tee1">
+  		<img src="img/acquire/2-11-14/lentrian-web_112.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/long-sleeve-top">
+  		<img src="img/acquire/2-11-14/lentrian-web_483.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/grey-wrap-vest">
+  		<img src="img/acquire/11-04-14/Lentrian-web-pictures-81.jpg">
+	</a>		
+	<a class="shop-item" href="http://store.lentrian.com/products/black-twisted-denim">
+  		<img src="img/acquire/2-11-14/lentrian-web_37.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/lss14-vst1">
+  		<img src="img/acquire/2-11-14/lentrian-web_18.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/long-cardigan-with-leather-details">
+  		<img src="img/acquire/2-11-14/lentrian-web_13-2.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/black-rough-linen-shirt-with-hidden-metal-zipper">
+  		<img src="img/acquire/11-04-14/Lentrian-web-pictures-65.jpg">
+	</a>	
+	<a class="shop-item" href="http://store.lentrian.com/products/double-layered-gatefold-cardigan">
+  		<img src="img/acquire/2-11-14/lentrian-web_475.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/grey-heavy-wool-coat-with-rough-texture">
+  		<img src="img/acquire/2-11-14/lentrian-web_55-2.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/rough-linen-shirt-with-hidden-metal-zipper-1">
+  		<img src="img/acquire/2-11-14/lentrian-web_111-2.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/grey-object-dyed-linen-pants">
+  		<img src="img/acquire/2-11-14/lentrian-web_49.jpg">
+	</a>		
+	<a class="shop-item" href="http://store.lentrian.com/products/heavy-wool-coat-with-rough-texture">
+  		<img src="img/acquire/2-11-14/lentrian-web_60-2.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/dyed-drop-crotch-linen-pants">
+  		<img src="img/acquire/2-11-14/lentrian-web_29.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/double-layered-jersey">
+  		<img src="img/acquire/2-11-14/lentrian-web_472.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/black-washed-leather-jacket">
+  		<img src="img/acquire/2-11-14/lentrian-web_30-2.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/grey-object-dyed-linen-pants">
+  		<img src="img/acquire/2-11-14/lentrian-web_44.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/heavy-wool-jacket-with-leather-center-front">
+  		<img src="img/acquire/2-11-14/lentrian-web_47-2.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/washed-black-leather-jacket">
+  		<img src="img/acquire/11-04-14/Lentrian-web-pictures-76.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/black-leather-wool-coat">
+  		<img src="img/acquire/2-11-14/lentrian-web_37-2.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/long-black-single-button-overcoat">
+  		<img src="img/acquire/2-11-14/lentrian-web_41-2.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/white-formal-shirt">
+  		<img src="img/acquire/2-11-14/lentrian-web_79.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/grey-overdyed-twisted-denim">
+  		<img src="img/acquire/11-04-14/Lentrian-web-pictures-12.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/hooded-double-layered-jersey">
+  		<img src="img/acquire/2-11-14/lentrian-web_466.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/double-layered-linen-coat">
+  		<img src="img/acquire/2-11-14/lentrian-web_13.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/hooded-double-layered-jersey">
+  		<img src="img/acquire/2-11-14/lentrian-web_460.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/black-long-linen-vest-with-high-collar">
+  		<img src="img/acquire/11-04-14/Lentrian-web-pictures-33.jpg">
+	</a>
+	<a class="shop-item" href="http://store.lentrian.com/products/asymmetrical-lamb-leather-jacket">
+  		<img src="img/acquire/2-11-14/lentrian-web_23-2.jpg">
+	</a>
 </section>
 <?php 
 include('inc/footer.php'); ?>
